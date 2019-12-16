df5c13b949f2500518e1da537ea49d673f22607c
Moonjunyoung <kt_sdsd@naver.com>
2019-12-16 11:17:15 +0900

    Updates

---
+++ precache-manifest.521d96430622b6c36973c88403033b7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f039cb9d3c21981b21ba55cb64b5a58",
+    "url": "/OriginalVersionDataView/index.html"
+  },
+  {
+    "revision": "6f2a383f297d682732d7",
+    "url": "/OriginalVersionDataView/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "943430f98ae6aa0eb34e",
+    "url": "/OriginalVersionDataView/static/css/main.4b719acd.chunk.css"
+  },
+  {
+    "revision": "6f2a383f297d682732d7",
+    "url": "/OriginalVersionDataView/static/js/2.3899ae04.chunk.js"
+  },
+  {
+    "revision": "943430f98ae6aa0eb34e",
+    "url": "/OriginalVersionDataView/static/js/main.486aeda2.chunk.js"
+  },
+  {
+    "revision": "70ef935ff52d7d91db17",
+    "url": "/OriginalVersionDataView/static/js/runtime-main.8a692609.js"
+  }
+]);
